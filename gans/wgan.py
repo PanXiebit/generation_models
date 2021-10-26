c331287cf23de3f36eeb21ec2e274b62e0d0f07e
PanXiebit <ftdpanxie@gmail.com>
2021-10-26 21:02:35 +0800

    wgan

--- gans/wgan.py
+++ gans/wgan.py
@@ -14,6 +14,7 @@ from torchvision.datasets import MNIST
 from torch.autograd import Variable
 
 
+
 class MNISTDataModule(LightningDataModule):
     def __init__(self, opts):
         super().__init__()
@@ -96,9 +97,7 @@ class Discriminator(nn.Module):
 
         return validity
 
-
-
-class GAN(LightningModule):
+class WGAN(LightningModule):
     def __init__(self, opts):
         super().__init__()
         self.opts = opts
@@ -107,56 +106,47 @@ class GAN(LightningModule):
 
         self.generator = Generator(img_size=opts.img_size, channels=opts.channels, latent_dim=opts.latent_dim)
         self.discriminator = Discriminator(img_size=opts.img_size, channels=opts.channels)
-        self.adversarial_loss = nn.MSELoss()
-
-    def forward(self, z):
-        return self.generator(z)
+        
     
     def training_step(self, batch, batch_idx, optimizer_idx):
         imgs, _ = batch
         # Adversarial ground truths
-        valid = Variable(torch.FloatTensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False).to(imgs.device)
-        fake = Variable(torch.FloatTensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False).to(imgs.device)
-
         z = Variable(torch.FloatTensor(np.random.normal(0, 1, (imgs.shape[0], self.latent_dim)))).to(imgs.device)
 
         real_imgs = imgs
-        gen_imgs = self.generator(z)
-
-        # train generator
+        
+        # train discriminator
         if optimizer_idx == 0:
-            
-            grid = torchvision.utils.make_grid(gen_imgs)
-            grid_x = torchvision.utils.make_grid(imgs)
-            self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
-            self.logger.experiment.add_image("original_images", grid_x, self.current_epoch)
+            fake_imgs = self.generator(z).detach()
 
-            g_loss = self.adversarial_loss(self.discriminator(gen_imgs), valid)
-
-            self.log('train/d_loss', g_loss, prog_bar=True)
-            return g_loss
-
-        else:
             real_logits = self.discriminator(real_imgs)
-            fake_logits = self.discriminator(gen_imgs.detach())
-            real_loss = self.adversarial_loss(real_logits, valid)
-            fake_loss = self.adversarial_loss(fake_logits, fake)
-            d_loss = 0.5 * (real_loss + fake_loss)
+            fake_logits = self.discriminator(fake_imgs)
+            d_loss = -torch.mean(real_logits) + torch.mean(fake_logits)
 
             self.log('train/d_loss', d_loss, prog_bar=True)
-            self.log('train/real_loss', real_loss, prog_bar=True)
             self.log('train/real_logits', real_logits.detach().mean(), prog_bar=True)
             self.log('train/fake_logits', fake_logits.detach().mean(), prog_bar=True)
             return d_loss
+        else:
+            if batch_idx % 5 == 0:
+                gen_imgs = self.generator(z)
+                g_loss = -torch.mean(self.discriminator(gen_imgs))
+                grid = torchvision.utils.make_grid(gen_imgs)
+                grid_x = torchvision.utils.make_grid(imgs)
+                self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
+                self.logger.experiment.add_image("original_images", grid_x, self.current_epoch)
+                self.log('train/g_loss', g_loss, prog_bar=True)
+
+                return g_loss
+            else:
+                return None
 
     def configure_optimizers(self):
         lr = self.opts.lr
-        b1 = self.opts.b1
-        b2 = self.opts.b2
 
-        opt_g = torch.optim.Adam(self.generator.parameters(), lr=lr, betas=(b1, b2))
-        opt_d = torch.optim.Adam(self.discriminator.parameters(), lr=lr, betas=(b1, b2))
-        return [opt_g, opt_d], []
+        opt_g = torch.optim.RMSprop(self.generator.parameters(), lr=lr)
+        opt_d = torch.optim.RMSprop(self.discriminator.parameters(), lr=lr)
+        return [opt_d, opt_g], [] # TODO, note the order.
 
 
 if __name__ == "__main__":
@@ -164,9 +154,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
     parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--lr", type=float, default=0.00005, help="adam: learning rate")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
     parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
@@ -174,7 +162,7 @@ if __name__ == "__main__":
     parser.add_argument("--sample_interval", type=int, default=1000, help="number of image channels")
     opt = parser.parse_args()
     # print(opt)
-    model = GAN(opt)
+    model = WGAN(opt)
     AVAIL_GPUS = min(1, torch.cuda.device_count())
     trainer = Trainer(gpus=AVAIL_GPUS, max_epochs=200, progress_bar_refresh_rate=20)
     data = MNISTDataModule(opt)
